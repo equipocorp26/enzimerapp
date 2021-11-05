d36f8e1d5de8f998801cb5a7d152a9aad899bbfa
Armando Campos <58012193+armando19993@users.noreply.github.com>
2021-11-04 20:02:14 -0500

    Agregando Modulo de Transacciones

---
+++ database/migrations/2021_11_05_010111_create_transacciones_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaccionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transacciones', function (Blueprint $table) {
+            $table->id();
+            $table->integer('monto');
+            $table->integer('status');
+            $table->date('fecha');
+            $table->string('codigo_pasarela');
+            $table->string('card_number');
+            $table->string('tipo_tarjeta');
+            $table->string('ip_pago');
+            $table->integer('id_usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transacciones');
+    }
+}
